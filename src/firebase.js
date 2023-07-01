0ebd0c8de54b292057df49930c16a783e5371f13
maximuslucas <lucaslimarueda@gmail.com>
2023-07-01 11:18:23 -0500

    actualizado

---
+++ src/firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// Import the functions you need from the SDKs you need
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCqlfYO8-DuZSWJv087EGI2i-ro-l1QIJE",
+  authDomain: "aplicacion-web-8a71e.firebaseapp.com",
+  projectId: "aplicacion-web-8a71e",
+  storageBucket: "aplicacion-web-8a71e.appspot.com",
+  messagingSenderId: "571791767097",
+  appId: "1:571791767097:web:1d18a770388d9c0fc37fe8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+const db = getFirestore(app)
+
+export {auth, db}
+export default app;
---
+++ src/views/IniciarSesion.vue
@@ -0,0 +1,37 @@
+<script setup> 
+ 
+import { reactive } from 'vue'; 
+ 
+const labelPosition = 'top'; 
+ 
+const formIniciarSesion = reactive({ 
+    correo: '', 
+    contrasena: '', 
+}) 
+ 
+</script> 
+<template> 
+    <div class="flex min-h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8"> 
+        <div class="sm:mx-auto sm:w-full sm:max-w-sm"> 
+            <img src="@/assets/cube-icon.svg" alt="logo vue" class="mx-auto h-20 w-auto"> 
+            <h2 class="mt-5 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Iniciar Sesión</h2> 
+            <div class="mt-6 sm:mx-auto sm:w-full sm:max-w-xs"> 
+                <el-form 
+                    :model="formIniciarSesion" 
+                    :label-position="labelPosition" 
+                    class="space-y-6" 
+                    > 
+                        <el-form-item label="Correo electrónico"> 
+                            <el-input type="email" v-model="formIniciarSesion.correo" clearable></el-input> 
+                        </el-form-item>  
+ 
+                        <el-form-item label="Contraseña"> 
+                            <el-input type="password" v-model="formIniciarSesion.pasword" clearable></el-input> 
+                        </el-form-item> 
+ 
+                    <el-button type="primary" class="w-full">Iniciar Sesión</el-button> 
+                </el-form> 
+            </div> 
+        </div> 
+    </div> 
+</template>
